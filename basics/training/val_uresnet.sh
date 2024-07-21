bd87d2a10e05db3c6e37eca39d30e41850965ffd
Francois Drielsma <francois.drielsma@gmail.com>
2024-07-20 20:59:49 -0700

    Updated training notebook

--- basics/training/val_uresnet.sh
+++ basics/training/val_uresnet.sh
@@ -10,7 +10,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=5
 #SBATCH --mem-per-cpu=4g
-#SBATCH --time=1:00:00 
+#SBATCH --time=0:15:00 
 #SBATCH --gpus a100:1
 
 singularity exec --bind /sdf/group/neutrino/drielsma/,/sdf/data/neutrino/ --nv /sdf/group/neutrino/images/develop.sif bash -c "python3 /sdf/data/neutrino/software/spine/bin/run.py -c /sdf/group/neutrino/drielsma/train/example/uresnet_val.cfg"
